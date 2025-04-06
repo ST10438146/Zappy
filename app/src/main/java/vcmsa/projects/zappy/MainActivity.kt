7b26ebb506441597bd19c740c7aeef16433fe322
lab_services_student <ST10438146@vcconnect.edu.za>
2025-04-06 09:14:58 +0200

    First commit

---
+++ app/src/main/java/vcmsa/projects/zappy/MainActivity.kt
@@ -0,0 +1,34 @@
+package vcmsa.projects.zappy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // ...
+        auth = Firebase.auth
+    }
+
+    fun signInWithEmailAndPassword(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    // ...
+                } else {
+                    // If sign in fails, display a message to the user.
+                    // ...
+                }
+            }
+    }
+}
